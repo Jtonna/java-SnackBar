0223146d495ff32f3be542ad2f2f2289f71f2cf2
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-28 14:10:51 -0700

    Update Snack.java

--- SnackbarAPP/src/snackbarApp/Snack.java
+++ SnackbarAPP/src/snackbarApp/Snack.java
@@ -3,17 +3,28 @@ package snackbarApp;
 public class Snack
 {
 	// Snack had the fields (so knows);
+	// int =  primitive data type, no decimal's
+	// string = self explanitory
+	// float = 4 bytes (54851.3285)
 	private static int maxId = 0;
 	private int id;
 	private String name;
 	private int quantity;
-	private Boolean cost;
+	private float cost;
 	private int vendingId;
 
 	// Snack has the methods (so can);
-	public Snack()
+	public Snack(String name, int quantity, float cost, int vendingId)
 	{
 		//so can goes here
+		maxId++;
+		id = maxId;
+
+		this.name = name;
+		this.quantity = quantity;
+		this.cost = cost;
+		this.vendingId = vendingId;
+		
 	}
 
 }
